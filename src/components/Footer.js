937a5fae3f61b451988b16a062a7b8b8e495164a
Miroslav Gurnyak <mirmiroslav@gmail.com>
2025-01-25 13:04:33 +0300

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-border"></div>
+      <div className="container">
+        <div className="footer-flex">
+          <div className="footer-copyright">2024 © Все права защищены</div>
+          <div className="links">
+            Telegram / Instagram / Vk / mail@nikitaparfenov.ru
+          </div>
+          <div className="footer-made-by">Сделано @vanyakuf</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
