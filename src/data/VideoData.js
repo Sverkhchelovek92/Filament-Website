867f01dfa45783fb65895930d2ffca8cffaeca91
Miroslav Gurnyak <mirmiroslav@gmail.com>
2025-02-03 11:39:27 +0300

    some stuff

---
+++ src/data/VideoData.js
@@ -0,0 +1,18 @@
+const VideoData = [
+  {
+    id: 1,
+    title: 'Acm',
+    category: 'Industry',
+    thumbnail: '001.png',
+    videoUrl: '',
+  },
+  {
+    id: 2,
+    title: 'Maket',
+    category: 'Industry',
+    thumbnail: '002.png',
+    videoUrl: '',
+  },
+]
+
+export default VideoData
